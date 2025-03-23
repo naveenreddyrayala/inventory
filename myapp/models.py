0542938ef3f7bb760083b799e9c963c3d9a3e37e
Naveen Rayala <naveenreddyrayala@gmail.com>
2025-03-23 19:54:04 +0530

    after todo

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
 from django.db import models
 
-# Create your models here.
+class TodoItem(models.Model):
+    title = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
